9025cd35c1ca9aea56c6b159b0a3cab295db4cfb
GarvJaiswal <garvjaiswal08@gmail.com>
2024-02-20 11:09:44 +0530

    [Garv] Add:Added a queries to implement string functions

---
+++ StringFunctions.sql
@@ -0,0 +1,23 @@
+SELECT LEN(Name)
+FROM employees;
+
+SELECT UPPER(Name)
+FROM employees;
+
+SELECT LOWER(Name)
+FROM employees;
+
+SELECT LTRIM(' Name')
+FROM employees;
+
+SELECT RTRIM('Name  ')
+FROM employees;
+
+SELECT SUBSTRING('MICROSOFT',6,9)
+FROM employees;
+
+SELECT REPLACE('MICROSOFT','MICRO','MAJOR')
+FROM employees;
+
+SELECT REPLICATE('DUMMY',5)
+FROM employees;
