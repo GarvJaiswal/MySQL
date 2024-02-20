5121d1430986612967caf4e20e74b36d6d3a24b0
GarvJaiswal <garvjaiswal08@gmail.com>
2024-02-20 11:04:00 +0530

    [Garv] Add:Added a queries to implement numeric functions

---
+++ NumericFunctions.sql
@@ -0,0 +1,19 @@
+SELECT ABS(SALARY)
+FROM employees
+WHERE Name='Garv';
+
+SELECT CEILING(SALARY)
+FROM employees
+WHERE Name='Garv';
+
+SELECT FLOOR(SALARY)
+FROM employees
+WHERE Name='Garv';
+
+SELECT SQUARE(SALARY)
+FROM employees
+WHERE Name='Garv';
+
+SELECT SQRT(SALARY)
+FROM employees
+WHERE Name='Garv';
